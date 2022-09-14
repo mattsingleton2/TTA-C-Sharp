6f07ae83d859ae282f9f6e59847007b511998626
Matthew Singleton <matt.singleton2@gmail.com>
2022-09-14 13:10:57 -0500

    Updated Exceptions to handle assignment.

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Exceptions
 {
@@ -7,32 +7,39 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            // Setting up try/catch block to deal with exceptions as they occur.
             try
             {
-                Console.WriteLine("Pick a number.");
-                int numberOne = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Pick a second number.");
-                int numberTwo = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Dividing the two....");
-                int numberThree = numberOne / numberTwo;
-                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
-                Console.ReadLine();
+                // Build a list of ints, ask the user for a divisor, and display a message showing the results.
+                List<int> ints = new List<int> { 10, 100, 1000, 10000, 50, 500, 50000, 17032 };
+                Console.WriteLine("We have a list of numbers - Please select a number and we'll divide all the list's numbers by your number.");
+                int userDivisor = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < ints.Count; i++)
+                {
+                    Console.WriteLine("\nDividing " + ints[i] + " by " + userDivisor + ".\nThis evaluates to " + ints[i]/userDivisor + ".");
+                    
+                }
             }
+            // Catch the first exception - receiving a string instead of an integer.
             catch (FormatException e)
             {
                 Console.WriteLine("Please type a whole number. Don't use letters.");
                 return;
             }
+            // Catch the second exception - user dividing by zero.
             catch (DivideByZeroException e)
             {
                 Console.WriteLine("You can't divide by zero...");
             }
+            // Catch any other exceptions that we didn't expect and just spit out the "scary" message for the user.
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            // Let's move on with life! 
             finally
             {
+                Console.WriteLine("We've left our try/catch block.");
                 Console.ReadLine();
             }
             
