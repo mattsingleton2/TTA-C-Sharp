1a9d2b87fa5163295e4822282d00b6575fb36c39
Matthew Singleton <matt.singleton2@gmail.com>
2022-09-15 13:04:04 -0500

    Created new Method_Calls Project.

---
+++ Method_Calls/Method_Calls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Method_Calls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Method_Calls/Method_Calls/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Calls
+{
+    internal class Vehicle
+    {
+    }
+}
