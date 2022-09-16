7f52bb24fb0cd3b165cd7896f2ffd12948841abc
Matthew Singleton <matt.singleton2@gmail.com>
2022-09-15 19:50:55 -0500

    Updated Method_Calls again for an optional parameter style method call.

---
+++ Method_Calls/Method_Calls/OptionalArgumentPractice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Calls
+{
+    internal class OptionalArgumentPractice
+    {
+        //  Establishing a class with an optional parameter instead of requiring it.
+        public int Multiplication(int num1, int num2 = 5)
+        {
+            try
+            {
+                return num1 * num2;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+
+    }
+}
--- Method_Calls/Method_Calls/Program.cs
+++ Method_Calls/Method_Calls/Program.cs
@@ -6,17 +6,25 @@ namespace Method_Calls
     {
         static void Main(string[] args)
         {
-            //  Take in some numbers.
-            Console.WriteLine("Hello! Please pick a number and we'll do addition, division and multiplication on them.");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            
             //  Can't use a class without instantiating an object for it, so let's do that.
             CustomMath cmathObject = new CustomMath();
 
-            //  Let's give the user what they asked for.
-            Console.WriteLine("Here's the addition Result: " + cmathObject.Addition(num1));
-            Console.WriteLine("Here's the division Result: " + cmathObject.Division(num1));
-            Console.WriteLine("Here's the multiplication Result: " + cmathObject.Multiplication(num1));
+            //  Take in some numbers.
+            Console.WriteLine("Hello! Please pick a number and we'll do addition, division and multiplication on them.");
+            try
+            {
+                //  Doing this because when I spammed my numbers to get through the program I hit a stack overflow exception, so just accounting for that.
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Here's the addition Result: " + cmathObject.Addition(num1));
+                Console.WriteLine("Here's the division Result: " + cmathObject.Division(num1));
+                Console.WriteLine("Here's the multiplication Result: " + cmathObject.Multiplication(num1));
+            }
+            catch (Exception e)
+            {
+                // This will just log that error message whether it's a stack overflow or just failed.
+                Console.WriteLine(e.Message);
+            }
+
             Console.WriteLine("\n------------BEGIN STEP 279------------\n");
 
 
@@ -44,6 +52,28 @@ namespace Method_Calls
 
             //  Overload # 3 method call. This is a string.
             Console.WriteLine("Overload Method Three Returned...: " + mmathObject.Operation(numOverload3));
+
+
+            Console.WriteLine("\n------------BEGIN STEP 280------------\n");
+
+            //  Main Method Submission Assignment Step 280 START
+            //  Begin by instantiating our new class object to handle optional parameter passing.
+            OptionalArgumentPractice optArg = new OptionalArgumentPractice();
+
+            //  Ask user to input 2 numbers, one at a time. Let them know the second number's optional.
+            Console.WriteLine("We need at least one integer, but you can put two if you'd like. If you don't want to put the second one in, simply hit Enter...");
+            int numOpt1 = Convert.ToInt32(Console.ReadLine());
+
+            //  Attempt to see if we can convert the second entry (or lack thereof) to an integer. If not, we'll just call the method with one argument.
+            try
+            {
+                int numOpt2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Here, I'm passing both numbers (Defaulting Value 5 if one wasn't entered for the second number: " + optArg.Multiplication(numOpt1, numOpt2));
+            }
+            catch
+            {
+                Console.WriteLine("Here, I'm only passing the first number and not even letting the method receive the second number or lack thereof: " + optArg.Multiplication(numOpt1));
+            }
 
         }
     }
