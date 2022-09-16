f2b7b1fc55e57fcc4c66d94355429cc20531ee27
Matthew Singleton <matt.singleton2@gmail.com>
2022-09-15 19:26:40 -0500

    Updated Method_Calls to support Assignment Step 279 (Multiple Overloads One Method)

---
+++ Method_Calls/Method_Calls/MultiMath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Calls
+{
+    internal class MultiMath
+    {
+        public int Operation(int num1)
+        {
+            return num1 + 1;
+        }
+
+        public int Operation(decimal num1)
+        {
+            //Converting the decimal input to an int since I have to return an int. Another way to do this is commented out below for a thorough submission.
+            return Convert.ToInt32(num1) * 2; 
+
+            //  Alternate way to handle this ... Instead of converting the input, I run the operation and convert the result of that operation's decimal to an integer instead.
+            //  decimal answer = num1 * 2;
+            //  return Convert.ToInt32(answer);
+                     
+        }
+
+        public int Operation(string num1)
+        {
+            //  Try to convert the string to an integer if possible. Using a Try/Catch block to display the error message instead if the conversion fails.
+            try
+            {
+               int modifiedStringAsInt = Convert.ToInt32(num1);
+               return modifiedStringAsInt / 2;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+        
+
+    }
+}
--- Method_Calls/Method_Calls/Program.cs
+++ Method_Calls/Method_Calls/Program.cs
@@ -17,6 +17,34 @@ namespace Method_Calls
             Console.WriteLine("Here's the addition Result: " + cmathObject.Addition(num1));
             Console.WriteLine("Here's the division Result: " + cmathObject.Division(num1));
             Console.WriteLine("Here's the multiplication Result: " + cmathObject.Multiplication(num1));
+            Console.WriteLine("\n------------BEGIN STEP 279------------\n");
+
+
+            // Main Method Submission Assingnment Step 279 START
+            //  Begin by instantiating the class with an object...
+            MultiMath mmathObject = new MultiMath();
+
+            //  Overload # 1 gathering
+            Console.WriteLine("Let's have an integer, please.");
+            int numOverload1 = Convert.ToInt32(Console.ReadLine());
+
+            //  Overload # 2 gathering
+            Console.WriteLine("Pick a decimal. For example, maybe 10.50");
+            decimal numOverload2 = Convert.ToDecimal(Console.ReadLine());
+
+            //  Overload # 3 gathering. 
+            Console.WriteLine("Finally, type a number as a string. For Example, Try using \"10\" and we won't modify it -- it will be left as a string.");
+            string numOverload3 = Console.ReadLine();
+
+            //  Overload # 1 method call. This is an int.
+            Console.WriteLine("Overload Method One Returned...: " + mmathObject.Operation(numOverload1));
+
+            //  Overload # 2 method call. This is a decimal.
+            Console.WriteLine("Overload Method Two Returned...: " + mmathObject.Operation(numOverload2));
+
+            //  Overload # 3 method call. This is a string.
+            Console.WriteLine("Overload Method Three Returned...: " + mmathObject.Operation(numOverload3));
+
         }
     }
 }
